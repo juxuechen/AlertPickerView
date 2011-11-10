0e2c0d30457b07edc9f0e7352c014b4423504390
juxuechen <juxue.cheny@gmail.com>
2011-11-10 11:21:44 +0800

    loading alert view

---
+++ PickerAlertView/LoadingAlertView.h
@@ -0,0 +1,13 @@
+//
+//  LoadingAlertView.h
+//  PickerAlertView
+//
+//  Created by chenyang on 11/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadingAlertView : UIAlertView
+
+@end
---
+++ PickerAlertView/LoadingAlertView.m
@@ -0,0 +1,23 @@
+//
+//  LoadingAlertView.m
+//  PickerAlertView
+//
+//  Created by chenyang on 11/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "LoadingAlertView.h"
+
+@implementation LoadingAlertView
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
