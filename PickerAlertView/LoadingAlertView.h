5b93cdb273d9b3dfdb7a763a5659126bed778aff
juxuechen <juxue.cheny@gmail.com>
2011-11-10 11:40:44 +0800

    loading alertview

--- PickerAlertView/LoadingAlertView.h
+++ PickerAlertView/LoadingAlertView.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface LoadingAlertView : UIAlertView
+@interface LoadingAlertView : UIAlertView 
 
 @end
--- PickerAlertView/LoadingAlertView.m
+++ PickerAlertView/LoadingAlertView.m
@@ -20,4 +20,32 @@
     return self;
 }
 
+- (id)initWithFrame:(CGRect)frame {
+	if (self = [super initWithFrame:frame]) {
+		UIActivityIndicatorView *activity = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(50, 50, 50, 50)];
+		[self addSubview:activity];
+		[activity release];
+	}
+	return self;
+}
+
+- (void)setFrame:(CGRect)rect {
+	[super setFrame:CGRectMake(0, 0, 320, 300)];//width 默认 284
+	self.center = CGPointMake(320/2, 280);
+}
+
+- (void)layoutSubviews {
+	[super layoutSubviews];
+	for (UIView *view in self.subviews) {
+		if (view.frame.size.height == 43) {
+			view.frame = CGRectMake(view.frame.origin.x, 232, 127, 43);
+		}
+	}
+}
+
+- (void)drawRect:(CGRect)rect {
+	[super drawRect:rect];
+}
+
+
 @end
